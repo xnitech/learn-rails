67dba4995c2fd9028a6c7c14e3e25f032f01b826
Mark Gruetzner <mark.gruetzner@gmail.com>
2016-07-04 13:41:31 -0500

    dynamic home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  #root to: redirect('/about.html')
+  root to: 'visitors#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
