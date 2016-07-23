917a7e6d500e0be56eb735c3d78666f691eb3b6c
Mark Gruetzner <mark.gruetzner@gmail.com>
2016-07-23 12:54:13 -0500

    mail list

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #post 'contact', to: 'contacts#process_form'
 
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
